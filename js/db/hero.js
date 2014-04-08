91d2c6535d55a03277f6ee85443e280b15ca3cc1
recchi <recchimail@gmail.com>
2014-04-08 21:47:37 +0400

    Refactor

--- js/db/hero.js
+++ js/db/hero.js
@@ -1,69 +1,59 @@
-var imgUrl = '../img'
-
-var _gHero = [ 
-    {
+var _gHero = {
+    'assassin': {
         'priority'   : '1',
         'heroClass'  : 'assassin',
         'heroName'   : 'Ассасин',
         'heroColor'  : false,
         'crown'      : false,
-        'texture'    : '../img/heroes/01_assasin.png'
     },
-    {
+    'thief': {
         'priority'   : '2', 
         'heroClass'  : 'thief',
         'heroName'   : 'Вор',
         'heroColor'  : false,
         'crown'      : false,
-        'texture'    : '../img/heroes/02_thief.png'
     },
-    {
+    'mage': {
         'priority'   : '3', 
         'heroClass'  : 'mage',
         'heroName'   : 'Чародей',
         'heroColor'  : false,
         'crown'      : false,
-        'texture'    : '../img/heroes/03_magician.png'
     },
-    {
+    'king': {
         'priority'   : '4', 
         'heroClass'  : 'king',
         'heroName'   : 'Король',
         'heroColor'  : 'yellow',
         'crown'      : true,
-        'texture'    : '../img/heroes/04_king.png'
     },
-    {
+    'bishop': {
         'priority'   : '5', 
         'heroClass'  : 'bishop',
         'heroName'   : 'Епископ',
         'heroColor'  : 'blue',
         'crown'      : false,
-        'texture'    : '../img/heroes/05_bishop.png'
     },
-    {
+    'merchant': {
         'priority'   : '6', 
         'heroClass'  : 'merchant',
         'heroName'   : 'Купец',
         'heroColor'  : 'green',
-        'crown'      : false,
-        'texture'    : '../img/heroes/06_merchant.png'                             
+        'crown'      : false,                          
     },
-    {
+    'architect': {
         'priority'   : '7', 
         'heroClass'  : 'architect',
         'heroName'   : 'Зодчий',
         'heroColor'  : false,
         'crown'      : false,
-        'texture'    : '../img/heroes/07_architect.png'
     },
-    {
+    'warlord': {
         'priority'   : '8', 
         'heroClass'  : 'warlord',
         'heroName'   : 'Кондотьер',
         'heroColor'  : 'red',
         'crown'      : false,
-        'texture'    : '../img/heroes/08_warlord.png'
     }
 
-];
+};
--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 (function() {
 var setting = {
-
+  
     }
-    ,shuffledDeck = []
-    ,shuffledHeroes = [];
+    ,heroes = [  'assassin'
+                ,'thief'
+                ,'mage'
+                ,'king'
+                ,'bishop'
+                ,'merchant'
+                ,'architect'
+                ,'warlord']
+    ,shuffledHeroes = []
+    ,deck = [];
 
-//Шаффл для 'var _gСards'
+//Создание колоды
+function createDeck() { 
+    for(var i = 0; i <=65; i++) {
+        deck.push('card_'+i);  
+    }
+    return deck;
+}
+
+//Шаффл
 Array.prototype.shuffle = function() {
     for (var z = 0; z < this.length; z++) {
         var a = this[z];
@@ -20,8 +36,8 @@ var extraHeroes = [];
 var trash = [];
 
 Array.prototype.removeExtraHeroes = function () { 
-    for (i = 0; i < 3; i++) {
-        if (i > 0 && this[0].heroClass == 'king') {
+    for (var i = 0; i < 3; i++) {
+        if (i > 0 && this[0] == 'king') {
             trash[i] = this.shift();
             this.push(trash[i]);
             extraHeroes[i] = this.shift();
@@ -34,65 +50,64 @@ Array.prototype.removeExtraHeroes = function () {
 };
 
 function heroShuffle () {
-    shuffledHeroes = $.extend(true, [], _gHero);
+    shuffledHeroes = $.extend(true, [], heroes);
     shuffledHeroes.shuffle();
     shuffledHeroes.removeExtraHeroes();
 }
+
 //Раздача карт
 function handingOut(arrDeck, arrPlayers) {
-    for (i = 0; i < arrPlayers.length; i++) {
+    for (var i = 0; i < arrPlayers.length; i++) {
         for (z = 0; z < 4; z++) {
             arrPlayers[i].options.hand[z] = arrDeck.shift();
         }
     }
 }
 
-//Visial - Показ Героев на экране
+//Visual - Показ Героев на экране
 Array.prototype._vShowMeHeroes = function() {
     for (var i = 0; i < this.length; i++) {
-        var id = this[i].heroClass;
-        var img = this[i].texture;
-        $('.hero_choice').append('<div class="hero_card '+id+'"></div>');
+        var id = this[i];
+        $('.hero-choice').append('<div class="card hero '+id+'"></div>');
     }
 }
 
-shuffledDeck = $.extend(true, [], _gDeck);
-shuffledDeck.shuffle();
+createDeck();
+deck.shuffle();
 
 //Конструктор игрока
 function Player(options) {
     this.options = {
-        'playerName'      : options.playerName || '',
-        'playerHero'      : options.playerHero || '',
-        'coins'           : options.coins || 2,
-        'hand'            : options.hand || [],
-        'field'           : options.field || [],
-        'playerStatus'    : options.playerStatus || '',
-        'quartalsOnField' : options.quartersOnField || '',
+         'playerName'      : options.playerName || ''
+        ,'playerHero'      : options.playerHero || ''
+        ,'coins'           : options.coins || 2
+        ,'hand'            : options.hand || []
+        ,'field'           : options.field || []
+        ,'playerStatus'    : options.playerStatus || ''
+        ,'quartersOnField' : options.quartersOnField || ''
     }
-
-    this.useHeroPower = function() {
+}
+//Способности игрока
+Player.prototype = {
+    useHeroPower: function() {
         var hClass = this.options.playerHero.heroClass;
         arg = ['first_param'];
         _gHeroPowers[hClass].apply(this, arg);
-    }
+    },
+    drawCard: function() {
 
-    this.drawCard = function() {
-
-    }
-
-    this.takeCoins = function() {
+    },
+    takeCoins: function() {
         this.options.coins += 2;
-    }
-
-    this.buildQuarter = function() {
+    },
+    buildQuarter: function() {
         
     }
 }
 
 var ivanOpt = {
     'playerName'        : 'Ivan',
-    'playerHero'        : _gHero[5]
+    'playerHero'        : _gHero['merchant']
 };
 var vasyaOpt = {
     'playerName'        : 'Vasya'
@@ -112,7 +127,7 @@ var valera = new Player(valeraOpt);
 var players = [];
 players.push(ivan, vasya, petya, valera);
 
-handingOut(shuffledDeck, players);
+handingOut(deck, players);
 
 ivan.useHeroPower();
 
@@ -120,7 +135,7 @@ ivan.useHeroPower();
 // Функция 
 function edTest() {
     // console.log(_gDeck);
-    console.log(shuffledDeck);
+    console.log(deck);
     // console.log(ivan);
     // console.log(ivan.options.hand.length);
     // console.log(_gDeck[0].cardColor);
@@ -130,20 +145,21 @@ function edTest() {
 };
 edTest();
 
-$('#lookAtHeroesButton').on('click', function(){
-        $('.hero_choice').empty('<div>');
+
+$('#choose-a-hero').on('click', function(){
+        $('.hero-choice').empty('<div>');
         heroShuffle();
         console.log(shuffledHeroes);
         shuffledHeroes._vShowMeHeroes();
 });
 
-$('.hero_choice').on('click', '.hero_card', function() {
-    var allHero = $('.hero_card');
+$('.hero-choice').on('click', '.hero', function() {
+    var allHero = $('.hero');
     var chosenHero = $(this);
-    if(!allHero.hasClass('card_highlighted')){
-        chosenHero.toggleClass('card_highlighted');
-    } else if(chosenHero.hasClass('card_highlighted')) {
-        chosenHero.toggleClass('card_highlighted');
+    if(!allHero.hasClass('hero-pick')){
+        chosenHero.toggleClass('hero-pick');
+    } else if(chosenHero.hasClass('hero-pick')) {
+        chosenHero.toggleClass('hero-pick');
     }
 });
 
